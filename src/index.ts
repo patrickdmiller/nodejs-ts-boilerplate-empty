67a24ad3a0d264d61b41626905507d9a2e622d4b
Patrick Miller <patrickmiller@gmail.com>
2023-07-08 20:45:29 -0400

    added root

--- src/index.ts
+++ src/index.ts
@@ -12,9 +12,10 @@ const app = express();
     // this.app.use(cors({ credentials: true, origin: corsOrigins }));
     app.use(express.json()); // Parse out our json
     app.use(cookieParser()); // Parse out our cookies
-
     app.use('/api/v1/', router);
-
+    app.get('/', (req: Request, res: Response)=>{
+      res.json({status:"success"})
+    })
   app.listen(PORT, '0.0.0.0', () => {
       console.error(`Presence Service http://0.0.0.0:${PORT}`);
     });
